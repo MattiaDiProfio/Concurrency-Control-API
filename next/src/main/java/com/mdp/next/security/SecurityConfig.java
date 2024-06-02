714b8692c0674a26d79e27de0820aa314f19609e
Mattia Di Profio <mattia.diprofio@gmail.com>
2024-06-02 15:33:23 +0100

    setup authentication login infrastructure

--- next/src/main/java/com/mdp/next/security/SecurityConfig.java
+++ next/src/main/java/com/mdp/next/security/SecurityConfig.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+
+import com.mdp.next.security.filter.AuthenticationFilter;
+
 import lombok.AllArgsConstructor;
 import org.springframework.security.config.http.SessionCreationPolicy;
 
@@ -15,6 +18,12 @@ public class SecurityConfig {
 @SuppressWarnings({ "deprecation", "removal" })
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        // change the url on which the attemptAuthenticate() method on the authenticationFilter 
+        // will be triggered from "/login" (default) to "/authenticate"
+        AuthenticationFilter authenticationFilter = new AuthenticationFilter();
+        authenticationFilter.setFilterProcessesUrl("/authenticate"); 
+
         http
             .headers(headers -> headers.frameOptions().disable())
             .csrf(csrf -> csrf.disable())
@@ -22,6 +31,7 @@ public class SecurityConfig {
                     .requestMatchers("/h2/**").permitAll() // New Line: allows us to access the h2 console without the need to authenticate. ' ** '  instead of ' * ' because multiple path levels will follow /h2.
                     .requestMatchers(HttpMethod.POST, SecurityConstants.REGISTER_PATH).permitAll()
                     .anyRequest().authenticated())
+                    .addFilter(authenticationFilter)
             .sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         return http.build();
     }
--- next/src/main/java/com/mdp/next/security/filter/AuthenticationFilter.java
+++ next/src/main/java/com/mdp/next/security/filter/AuthenticationFilter.java
@@ -1,5 +1,35 @@
 package com.mdp.next.security.filter;
 
-public class AuthenticationFilter {
-    
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mdp.next.entity.User;
+
+public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        try {
+
+            // maps the request payload field to a user object - username, password
+            User user = new ObjectMapper().readValue(request.getInputStream(), User.class);
+
+            // pass the credentials into the authentication manager
+
+        } catch (IOException e) {
+            // exception handler will only work if the request is being handled by the dispatcher servlet, 
+            // since this request is not ***, a runtime exception will not be caught hence why we gotta specify a 
+            // custom filter exception handler
+            // *** spring security filters are executed before the dispatcher servlet
+            throw new RuntimeException();
+        }
+        return super.attemptAuthentication(request, response);
+    }    
+
 }
