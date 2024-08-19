85c51295811e3aa8b02a2b8c9d351542a33085e6
Mattia Di Profio <mattia.diprofio@gmail.com>
2024-08-19 12:07:29 +0100

    complete UserService unit tests

--- next/src/test/java/com/mdp/next/security/user/UserIntegrationTests.java
+++ next/src/test/java/com/mdp/next/security/user/UserIntegrationTests.java
@@ -4,4 +4,33 @@ public class UserIntegrationTests {
 
     // TODO : simulate all requests in the Controller layer, including authorization and access to resources
 
+    /*
+     * setup method
+     * - register and login 2 different users, 1 admin and 1 customer
+     *
+     * endpoints to test are :
+     *
+     * 1. GET /user/all
+     * - attempt to send request with customer -> fails due to missing ADMIN role
+     * - attempt to send request with admin -> assert list returned is of length 2, and that a customer and admin are present
+     *
+     * 2. GET /user/userID
+     * - attempt to get /user/1 and 2 with ADMIN -> both succeed
+     * - attempt to get /user/1 with customer -> fails
+     * - attempt to get /user/2 with customer -> success since they own the profile
+     *
+     * 3. PUT /user/userID
+     * - attempt to edit user 1 with customer -> fails
+     * - attempt to edit user 2 with admin -> fails
+     * - attempt to update email on user 2 with customer -> success since the customer owns this profile
+     *
+     * 4. DELETE /user/userID
+     * - attempt to delete user 1 with customer -> fails
+     * - attempt to delete user 2 with admin -> fails
+     * - attempt to delete user 1 with admin -> success since it's the admin's profile
+     *
+     * teardown method
+     * - logout the remaining logged-in users, 1 admin
+     *
+     * */
 }
--- next/src/test/java/com/mdp/next/security/user/UserUnitTests.java
+++ next/src/test/java/com/mdp/next/security/user/UserUnitTests.java
@@ -1,7 +1,105 @@
 package com.mdp.next.security.user;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import com.mdp.next.entity.User;
+import com.mdp.next.entity.UserDTO;
+import com.mdp.next.repository.UserRepository;
+import com.mdp.next.service.UserServiceImpl;
+
+@RunWith(MockitoJUnitRunner.class)
 public class UserUnitTests {
 
-    // todo : test the functionality of methods supplied in the Service layer for the users
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @InjectMocks
+    private UserServiceImpl userService;
+
+    @Test
+    public void testGetUserById() {
+        Optional<User> user = Optional.of(new User("Mattia", "mattia@gmail.com", "1 Fake Road", "mattia123", "password123", "ADMIN"));
+        when(userRepository.findById(1L)).thenReturn(user);
+
+        User unwrappedUser = UserServiceImpl.unwrapUser(user, 1L);
+        UserDTO fetchedUser = userService.getUser(1L);
+
+        assertEquals(unwrappedUser.getUserId(), fetchedUser.getID());
+        assertEquals(unwrappedUser.getName(), fetchedUser.getName());
+        assertEquals(unwrappedUser.getEmail(), fetchedUser.getEmail());
+        assertEquals(unwrappedUser.getUsername(), fetchedUser.getUsername());
+    }
+
+    @Test
+    public void testGetUsers() {
+        when(userRepository.findAll()).thenReturn(Arrays.asList(
+                new User("Mattia", "mattia@gmail.com", "1 Fake Road", "mattia123", "password123", "ADMIN"),
+                new User("John", "john@email.com", "2 Common Street", "john456", "password456", "BASE"),
+                new User("Mary", "mary@yahoo.com", "3 Random Avenue", "mary789", "password789", "BASE")
+        ));
+
+        List<UserDTO> allUsers = userService.getUsers();
+
+        assertEquals(allUsers.size(), 3);
+        assertEquals(allUsers.get(0).getName(), "Mattia");
+        assertEquals(allUsers.get(1).getEmail(), "john@email.com");
+    }
+
+    @Test
+    public void testCreateUser() {
+        // verify that the "createUser" method triggers the repository's "save" method
+        User newUser = new User("Mattia", "mattia@gmail.com", "123 Road Avenue", "mattia123", "password123", "ADMIN");
+        when(bCryptPasswordEncoder.encode(newUser.getPassword())).thenReturn("encodedPassword123");
+        userService.createUser(newUser);
+        assertEquals(newUser.getPassword(), "encodedPassword123");
+        verify(userRepository, times(1)).save(newUser);
+    }
+
+    @Test
+    public void testGetUserByUsername() {
+        Optional<User> user = Optional.of(new User("Mattia", "mattia@gmail.com", "1 Fake Road", "mattia123", "password123", "ADMIN"));
+        when(userRepository.findByUsername("mattia123")).thenReturn(user);
+
+        User unwrappedUser = UserServiceImpl.unwrapUser(user, 404L);
+        User fetchedUser = userService.getUser("mattia123");
+
+        assertNull(fetchedUser.getUserId()); // the id returned by this method is null by default
+        assertEquals(unwrappedUser.getName(), fetchedUser.getName());
+        assertEquals(unwrappedUser.getEmail(), fetchedUser.getEmail());
+        assertEquals(unwrappedUser.getUsername(), fetchedUser.getUsername());
+    }
+
+    @Test
+    public void testUpdateUser() {
+        Optional<User> user = Optional.of(new User("Mattia", "mattia@gmail.com", "1 Fake Road", "mattia123", "password123", "ADMIN"));
+        when(userRepository.findById(1L)).thenReturn(user);
+        User unwrappedUser = UserServiceImpl.unwrapUser(user, 1L);
+        userService.updateUser(unwrappedUser.getEmail(), "2 Common Street", 1L);
+        verify(userRepository, times(1)).save(unwrappedUser);
+    }
+
+    @Test
+    public void testDeleteUser() {
+        Optional<User> user = Optional.of(new User("Mattia", "mattia@gmail.com", "1 Fake Road", "mattia123", "password123", "ADMIN"));
+        when(userRepository.findById(1L)).thenReturn(user);
+        User unwrappedUser = UserServiceImpl.unwrapUser(user, 1L);
+        userService.deleteUser(1L);
+        verify(userRepository, times(1)).deleteById(1L);
+    }
 
 }
