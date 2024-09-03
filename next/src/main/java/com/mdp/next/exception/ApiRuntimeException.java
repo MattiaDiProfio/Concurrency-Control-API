80d13beab7d88a0762488e81a85777ecd9827af3
Mattia Di Profio <mattia.diprofio@gmail.com>
2024-09-03 21:11:52 +0100

    final refactor

--- next/src/main/java/com/mdp/next/exception/ApiRuntimeException.java
+++
@@ -1,9 +0,0 @@
-package com.mdp.next.exception;
-
-public class ApiRuntimeException extends RuntimeException {
-
-    public ApiRuntimeException(String message, Object... args) {
-        super(args.length > 0 ? String.format(message, args) : message);
-    }
-
-}
